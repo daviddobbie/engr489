917a8da2ced9409284a202f807975a11e5518904
David Dobbie <david.dobbie@ecs.vuw.ac.nz>
2018-04-04 11:55:38 +1200

    Created 1D example of extracting added exponentials, convert tau2 to T2 data

--- paper1_recreate/extractExponential.m
+++ paper1_recreate/extractExponential.m
@@ -7,24 +7,50 @@
 %Aim: Extract the exponential time constants from a summation of several
 %       type of them.
 
+
+
+
 clc
 clf
 clear
 
+%% Paul Teal FLINT init variables
+N1 = 50;       % number of data points in each dimension
+N2 = 1000;
 
-time_const = 0.1 : 0.1 : 4;
+Nx = 100;      % number of bins in relaxation time grids
+Ny = 101;      
 
-time_constants = [-6, -2];
-exp_weightings = [-1, 3];
+tau1min = 1e-4;
+tau1max = 10;
+deltatau2 = 3.5e-4;
 
-time = 0:0.2:20;
+T1 = logspace(-2,1,Nx); %form T1 domain, use log since will be small
+T2 = logspace(-2,1,Ny); %form T2 domain, use log since will be small
+
+[T2a,T1a] = meshgrid(log10(T2),log10(T1));
+
+%forms measurement arrays, time tau1 and tau2 domains
+tau2 = (1:N2)'*deltatau2;  
+% measurement time array (note, no t=0 data available)
+tau1 = logspace(log10(tau1min),log10(tau1max),N1)'; 
+
+K2 = exp(-tau2 * (1./T2) );     % simple T2 relaxation data
+K1 = 1-2*exp(-tau1 *(1./T1) );  % T1 relaxation data
+%%
+
+
+time_constants = [-0.2, -0.05];
+exp_weightings = [1, 4];
+
+time = tau2;
 init_individual_exp = [];
 
 
 
 
 for tc= time_constants
-    init_individual_exp = [init_individual_exp; exp(time/tc)];
+    init_individual_exp = [init_individual_exp; exp(time/tc)'];
     %%init_individual_exp = [init_individual_exp; normpdf(time, tc, 0.15) ];
 
 end
@@ -35,32 +61,30 @@ end
 data= sum(init_individual_exp); %adds exponetial functions together
 
 
-K_1 = exp(time/(-6));
 
-K_2 = ones(size(data'));
 
-%m = (K_1.*data)'; %for pdf of the it
+%m = (K1.*data)'; %for pdf of the it
 
-%K_0 = kron(K_1, K_2);
+%K0 = kron(K1, K2);
 
-K_0 = K_1;
+K0 = K2;
 
 % generate the noise
 noise_mean = 0;
 noise_std_dev = 0.05;
 noise = normrnd(noise_mean, noise_std_dev, 1,length(time));
-
+%m = data'
 m = (noise + data)';
 
 figure(1)
 hold on
 plot(time, m,'r');
-plot (time,K_1,'b');
+%%plot (time,K2,'b');
 plot(time,data, 'g');
 hold off
-legend("measured","kernel","noiseless data");
+legend("measured","noiseless data");
 title('Measured Signal Function (Sum of different exponentials)')
-xlabel('Time constant [s]')
+xlabel('Time [s]')
 ylabel('Amplitude')
 
 
@@ -87,12 +111,12 @@ f = c;
 title('c_r vector changing at each iteration');
 %this is the method that prevents it being divergent
 for i=1:20
-    %k_square = K_0*K_0'; 
-    stepFnMatrix = (heaviside(c.*K_0)).* eye(size(K_0'*K_0));
-    k_square = K_0 *  stepFnMatrix * K_0';       %recreate eq 30
+    %k_square = K0*K0'; 
+    stepFnMatrix = (heaviside(K0'*c)).* eye(Ny,Ny);
+    k_square = K0 *  stepFnMatrix * K0';       %recreate eq 30
     %made symmetric and semi-positive definite
     c = inv(k_square + alpha*eye(size(k_square)))*m; %eq 29
-    plot(time, (K_0.*c')')
+    plot(time, c)
 
     alpha =  sqrt(size(c,2)) / norm(c); %implement eq 41
     
@@ -104,9 +128,9 @@ hold off
 %{
 %this is the divergent method
 for i=1:50   
-    f = K_0'*c;
+    f = K0'*c;
     
-    c = (K_0 * f - m)/(-alpha);
+    c = (K0 * f - m)/(-alpha);
     
     plot(time, c)
     
@@ -117,22 +141,23 @@ for i=1:50
     
 end
 %}
-f = (K_0.*c')';
+f = K0'*c;
 
-var = K_0'*((K_0*K_0' + alpha*eye(size(k_square)))^-2)*K_0;
+var = K0'*((K0*K0' + alpha*eye(size(k_square)))^-2)*K0;
 
 
 figure(3)
 clf
 hold on
-plot(time, f,'r')
+set(gca, 'XScale', 'log')
+plot(T2, 10.^(f)./2,'r')
 hold off
 legend("Density Function")
-title('Probability Density Function for x')
-ylabel('f(x)')
-xlabel('x')
+title('Probability Density Function for T_2')
+ylabel('f(T_2)')
+xlabel('T_2')
 
-trapz(time,f)
+trapz(log10(T2), 10.^(f)./2)
 
 figure(4)
 plot(alpha_hist)
